2979c5d43e42a6ef48b546a45804024b58b4debc
Chase Ridenour <cridenour@tgen.org>
2022-09-09 14:52:15 -0400

    updated config and add setup.py

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='WNVHeppNextstrain',
+    version='0.0.1',
+    packages=[''],
+    url='https://github.com/CRideTGen/WNVHeppNextstrain',
+    license='',
+    author='Chase Ridenour',
+    author_email='cridenour@tgen.org',
+    description=''
+)
